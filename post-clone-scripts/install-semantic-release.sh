bf7b6491713f44dd1bbfc151b6d868f3fa548822
Lucas Santos <lhs.santoss@gmail.com>
2022-02-13 19:45:11 +0100

    feat(script): add prompt to repo and username

--- post-clone-scripts/install-semantic-release.sh
+++ post-clone-scripts/install-semantic-release.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
+read -rp "Your git username: " GIT_USERNAME
+sed -i '' "s+khaosdoctor+$GIT_USERNAME+g" package.json
+
+read -rp "Your repository name: " REPO_NAME
+sed -i '' "s+template-node-ts+$REPO_NAME+g" package.json
+
+npx husky install
 npx semantic-release-cli setup
