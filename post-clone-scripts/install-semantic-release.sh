6c3a75727d20b0526689afecb2de43f5f184591b
Wesley Matos <wm4tos.777@gmail.com>
2022-05-01 00:25:19 -0300

    chore: removing argument

--- post-clone-scripts/install-semantic-release.sh
+++ post-clone-scripts/install-semantic-release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 read -rp "Your git username: " GIT_USERNAME
-sed -i '' "s+khaosdoctor+$GIT_USERNAME+g" package.json
+sed -i "s+khaosdoctor+$GIT_USERNAME+g" package.json
 
 read -rp "Your repository name: " REPO_NAME
-sed -i '' "s+template-node-ts+$REPO_NAME+g" package.json
+sed -i "s+template-node-ts+$REPO_NAME+g" package.json
 
 npx husky install
 npx semantic-release-cli setup
